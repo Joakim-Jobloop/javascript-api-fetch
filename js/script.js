f7563fd921bbab0c79f08727b759f4525246fd41
Joakim-Jobloop <Joakim@jobloop.no>
2024-11-07 15:25:36 +0100

    *

--- js/script.js
+++ js/script.js
@@ -2,23 +2,30 @@
 const content = document.getElementById("content")
 
 
-
 async function getData() {
-    const response = await fetch("https://ghibliapi.vercel.app/films")
+
+    // first layer fetch:
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=386")
     const data = await response.json()
-    console.log(data)
-    // console.log(data[0].title)
-    // const allTitles = data.map((movie) => {
-    //     console.log(movie.title)
-    // })
+    // console.log(data.results)
+  
+    // second layer fetch
+    const pokemonUrls = data.results.map((pokemon)=> pokemon.url)
+    const pokemonPromises = pokemonUrls.map((url)=> fetch(url).then((response)=> response.json()))
+    
+    const pokeDetails = await Promise.all(pokemonPromises);
+
+    // console.log(pokemonUrls)
+    console.log(pokeDetails)
 
 
-    content.innerHTML = (data.map((movie) => {
+
+    content.innerHTML = (pokeDetails.map((pokemon) => {
         return (
             `
-        <div class="movie-card"> 
-          <h2>${movie.title}</h2>
-          <img src="${movie.image}" />
+        <div class="pokemon-card"> 
+          <h2>${pokemon.name}</h2>
+          <img src="${pokemon.sprites.front_default}"/>
         </div>
         
         
@@ -28,3 +35,31 @@ async function getData() {
 
 }
 getData()
+
+
+
+// async function getData() {
+//     const response = await fetch("https://ghibliapi.vercel.app/films")
+//     const data = await response.json()
+//     console.log(data)
+//     // console.log(data[0].title)
+//     // const allTitles = data.map((movie) => {
+//     //     console.log(movie.title)
+//     // })
+
+
+//     content.innerHTML = (data.map((movie) => {
+//         return (
+//             `
+//         <div class="movie-card"> 
+//           <h2>${movie.title}</h2>
+//           <img src="${movie.image}" />
+//         </div>
+        
+        
+//         `
+//         )
+//     }).join(" "))
+
+// }
+// getData()
